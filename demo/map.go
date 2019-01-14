4f8ca758f1a71e665e0ad6fe320658f80a2eb628
undancer <undancer@gmail.com>
2019-01-15 00:44:31 +0800

    添加简单的map操作demo

--- demo/map.go
+++ demo/map.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	var kvMap map[string]string = make(map[string]string)
+
+	kvMap["China"] = "Beijing"
+
+	for k, v := range kvMap {
+		fmt.Println(k, v)
+	}
+
+	value, ok := kvMap["中国"]
+	if ok {
+		fmt.Println("存在", value)
+	} else {
+		fmt.Println("不存在")
+	}
+
+	fmt.Println(kvMap)
 
 }
