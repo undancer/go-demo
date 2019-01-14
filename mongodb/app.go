fac3fe77ae1d7dffcdb8731748aadda28de40dc2
undancer <undancer@gmail.com>
2019-01-14 17:35:48 +0800

    操作mongodb

--- mongodb/app.go
+++ mongodb/app.go
@@ -1,11 +1,48 @@
 package mongodb
 
 import (
+	"context"
 	"fmt"
+	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/mongo"
+	"github.com/mongodb/mongo-go-driver/mongo/options"
+	"log"
+	"time"
+)
+
+const (
+	url        = "mongodb://localhost:27017"
+	database   = "go-database"
+	collection = "go-collection"
+	username   = "mongo"
+	password   = "mongo"
 )
 
 func Main() {
-	client, _ := mongo.NewClient("mongodb://localhost:27017")
-	fmt.Println(client)
+	var (
+		ctx    context.Context
+		client *mongo.Client
+		err    error
+	)
+
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+
+	if client, err = mongo.Connect(ctx, url, nil); err != nil {
+		log.Println(err.Error())
+	}
+	_database := client.Database(database)
+	_collection := _database.Collection(collection)
+	var limit int64 = 10
+	cur, err := _collection.Find(ctx, bson.M{}, &options.FindOptions{Limit: &limit})
+	if err != nil {
+		log.Println(err.Error())
+	}
+	defer cur.Close(ctx)
+
+	for cur.Next(ctx) {
+		var result bson.M
+		cur.Decode(&result)
+		fmt.Println(result)
+	}
+
 }
