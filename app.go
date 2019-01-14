62d345cea8a3746816be8a716800e29ddbb99d48
undancer <undancer@gmail.com>
2019-01-14 11:19:54 +0800

    go文件的相互引用

--- app.go
+++ app.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/undancer/go-demo/rabbitmq"
+)
 
 func main() {
 	fmt.Println("hello")
+	rabbitmq.Main()
 }
---
+++ rabbitmq/app.go
@@ -0,0 +1,7 @@
+package rabbitmq
+
+import "fmt"
+
+func Main() {
+	fmt.Println("RabbitMQ")
+}
